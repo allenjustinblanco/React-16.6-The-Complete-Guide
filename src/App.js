982c3c494789f169b5bb520d0d77317fac90b9f2
allenjustinblanco <blanco.allenjustin@gmail.com>
2018-12-21 19:48:11 -0600

    Added switchNameHandler function

--- src/App.js
+++ src/App.js
@@ -11,12 +11,18 @@ class App extends Component {
             {name: 'Max', age: 26},
             {name: 'Jennifer', age: 24}
             ]
-    }
+    };
+
+    switchNameHandler = () => {
+        console.log('clicked')
+    };
 
   render() {
     return (
       <div className="App">
         <h1>Hi im a React App</h1>
+          <p>This is really working!</p>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
           <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
           <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Dancing</Person>
           <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
