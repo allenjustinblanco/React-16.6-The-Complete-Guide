bea2d8a7a32d17815202ffa054cfd5d40b12a432
allenjustinblanco <blanco.allenjustin@gmail.com>
2018-12-21 18:52:09 -0600

    Added Person component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi im a React App</h1>
-    //   </div>
-    // );
-      return React.createElement('div', {className: 'App'}, '', React.createElement('h1', null, 'This is a React App'))
+    return (
+      <div className="App">
+        <h1>Hi im a React App</h1>
+          <Person/>
+      </div>
+    );
+    //   return React.createElement('div', {className: 'App'}, '', React.createElement('h1', null, 'This is a React App'))
   }
 }
 
