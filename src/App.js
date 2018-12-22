3f5dc8c05546e9bde6df06405f492dae41c643dd
allenjustinblanco <blanco.allenjustin@gmail.com>
2018-12-21 19:43:39 -0600

    Refactored App using state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+    //state only available in components which are used by extending Component in reacr
+    state = {
+        persons: [
+            {name: 'Joey', age: 23},
+            {name: 'Max', age: 26},
+            {name: 'Jennifer', age: 24}
+            ]
+    }
+
   render() {
     return (
       <div className="App">
         <h1>Hi im a React App</h1>
-          <Person name={'Max'} age={'43'}/>
-          <Person name={'Justin'} age={'25'}/>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Dancing</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
     //   return React.createElement('div', {className: 'App'}, '', React.createElement('h1', null, 'This is a React App'))
