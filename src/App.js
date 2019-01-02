92431964cba5886e87e5a57c64e3f337ae05e45e
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-02 15:40:33 -0600

    Added Radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import Person from './Person/Person'
+import Person from './Person/Person';
+import Radium from 'radium';
 
 
 class App extends Component {
@@ -63,7 +64,11 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
+            cursor: 'pointer',
+            ':hover': {
+              backgroundColor: 'lightgreen',
+              color: 'black'
+            }
         };
 
         let persons = null;
@@ -83,6 +88,10 @@ class App extends Component {
                 </div>
             )
             style.backgroundColor = 'red'
+            style[':hover'] = {
+              backgroundColor: 'cyan',
+              color: 'red'
+            }
         }
 
         let classes = [];
@@ -107,4 +116,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
