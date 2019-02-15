67bd6a5d5a2d34debefa8852e5328a852f86a681
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-02-14 18:54:09 -0600

    Enabled CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import styling from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -82,18 +82,18 @@ class App extends Component {
             style.backgroundColor = 'red'
         }
 
-        let classes = [];
+        const assignedClasses = [];
         if(this.state.persons.length <= 2){
-          classes.push('blue');
+          assignedClasses.push( styling.blue );
         }
         if(this.state.persons.length <= 1) {
-          classes.push('bold');
+          assignedClasses.push( styling.bold );
         }
 
         return (
-            <div className="App">
+            <div className={styling.App}>
                 <h1>Hi im a React App</h1>
-                <p className={classes.join(' ')}>This is really working!</p>
+                <p className={assignedClasses.join(' ')}>This is really working!</p>
                 <button style={style}
                         onClick={this.togglePersonsHandler}>Toggle Persons
                 </button>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './Person.css';
+import styling from './Person.css';
 
 const person = (props) => {
 
     return (
-        <div className={'Person'} style={style}>
+        <div className={styling.Person}>
             <p onClick={props.click}>I'm a {props.name} and I am {props.age}!</p>
             <p>{props.children}</p>
             <input type='text' onChange={props.changed} value={props.name}/>
